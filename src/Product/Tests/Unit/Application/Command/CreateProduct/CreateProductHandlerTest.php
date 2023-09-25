226fcc75411384bf4606c2492534b9f76e7cece2
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2023-09-25 17:10:47 +0200

    add first test to src folder

---
+++ src/Product/Tests/Unit/Application/Command/CreateProduct/CreateProductHandlerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Tests\Unit\Application\Command\CreateProduct;
+
+use PHPUnit\Framework\TestCase;
+
+final class CreateProductHandlerTest extends TestCase
+{
+    public function testShouldFail(): void
+    {
+        self::assertTrue(true);
+    }
+}
